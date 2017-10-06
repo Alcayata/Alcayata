fa04a7bf1c22db76f5931ed3a185c86cf996627c
Alcayata <32228573+Alcayata@users.noreply.github.com>
2017-10-05 23:20:12 -0500

    Nueva imagen Splash funcionalidad fragments en desarrollo

--- app/src/main/java/com/senamoviles/alcayata/alcayata/MainActivity.java
+++ app/src/main/java/com/senamoviles/alcayata/alcayata/MainActivity.java
@@ -1,13 +1,83 @@
 package com.senamoviles.alcayata.alcayata;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import com.jaredrummler.materialspinner.MaterialSpinner;
+import com.senamoviles.alcayata.alcayata.MainFragments.FloresFragment;
+import com.senamoviles.alcayata.alcayata.MainFragments.InfoFragment;
+import com.senamoviles.alcayata.alcayata.MainFragments.ModeloFragment;
+import com.ss.bottomnavigation.BottomNavigation;
+import com.ss.bottomnavigation.events.OnSelectedItemChangeListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.spinner)
+    MaterialSpinner spinner;
+    @BindView(R.id.frame_fragment_containers)
+    FrameLayout frameFragmentContainers;
+    @BindView(R.id.botton_nav)
+    BottomNavigation bottonNav;
+    Fragment fragment;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        bottonNav.setDefaultItem(2);
+        spinner.setItems("San Juan Bautista","El Crucifijo","La Virgen de los Dolores");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+
+            }
+        });
+
+        bottonNav.setOnSelectedItemChangeListener(new OnSelectedItemChangeListener() {
+            @Override
+            public void onSelectedItemChanged(int i) {
+                switch (i){
+                    case R.id.item_3d:
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_fragment_containers,new ModeloFragment());
+                        fragment = new ModeloFragment();
+                        break;
+                    case R.id.item_paso:
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_fragment_containers,new InfoFragment());
+                        fragment = new InfoFragment();
+                        break;
+                    case R.id.item_flower:
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_fragment_containers,new FloresFragment());
+                        fragment = new FloresFragment();
+                        break;
+                    case R.id.item_volume:
+                        // TODO 1: decidir que agregar
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_fragment_containers,new ModeloFragment());
+                        fragment = new ModeloFragment();
+                        break;
+                    case R.id.item_citial:
+                        // TODO 2: decidir que agregar
+                        transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_fragment_containers,new InfoFragment());
+                        fragment = new InfoFragment();
+                        break;
+
+                }
+                transaction.commit();
+            }
+        });
     }
 }
